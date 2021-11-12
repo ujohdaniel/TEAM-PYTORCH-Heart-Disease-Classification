5cb83fc5066c6dc0c3cc52473dfb4cfa76d6e3f5
ujohdaniel <67933561+ujohdaniel@users.noreply.github.com>
2021-11-12 00:20:05 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,38 @@
 from flask import Flask, render_template, request, redirect
 import model
 
-app = Flask(__name__)
-@app.route('/', methods= ['GET', 'POST'])
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/', methods= ['POST'])
 def predict():
 
-    if request.method == 'POST':
-        age = request.form['age']
-        sex = request.form['sex']
-        cp = request.form['cp']
-        trestbps = request.form['trestbps']
-        chol = request.form['chol']
-        fbs = request.form['fbs']
-        restecg = request.form['restecg']
-        thalach = request.form['thalach']
-        exang = request.form['exang']
-        oldpeak = request.form['oldpeak']
-        slope = request.form['slope']
-        ca = request.form['ca']
-        thal = request.form['thal']
 
-        model_pred = model.predict(age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal)
+    age = request.form['age']
+    sex = request.form['sex']
+    cp = request.form['cp']
+    trestbps = request.form['trestbps']
+    chol = request.form['chol']
+    fbs = request.form['fbs']
+    restecg = request.form['restecg']
+    thalach = request.form['thalach']
+    exang = request.form['exang']
+    oldpeak = request.form['oldpeak']
+    slope = request.form['slope']
+    ca = request.form['ca']
+    thal = request.form['thal']
 
-    return render_template('index.html', model_pred=model_pred)
+    prediction = model.predict_model(
+                age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal
+                )
+
+    if prediction== 1:
+        prediction ="positive. See your doctor!"
+    else:
+        prediction ="negative. You are alright!"
+
+    return render_template('index.html', prediction=prediction)
 
 
 if __name__ == '__main__':
